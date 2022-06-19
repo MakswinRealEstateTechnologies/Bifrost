bb3482dd58d869eb67ff243bf8bebb717a7d5a13
Alkın Çakıralar <a.cakiralar@gmail.com>
2022-06-20 00:30:25 +0300

    a

--- androidTestApp/src/main/java/com/makswin/androidtestapp/MainActivity.kt
+++ androidTestApp/src/main/java/com/makswin/androidtestapp/MainActivity.kt
@@ -6,7 +6,9 @@ import android.util.Log
 import android.widget.EditText
 import android.widget.TextView
 import androidx.lifecycle.lifecycleScope
-import com.makswin.bifrost.modules.misc.MiscellaneousRepository
+import com.makswin.bifrost.core.models.ResponseStatus
+import com.makswin.bifrost.modules.authentication.AuthenticationService
+import com.makswin.bifrost.modules.authentication.requestModels.Login
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
@@ -22,9 +24,37 @@ class MainActivity : AppCompatActivity() {
         this.asd = findViewById(R.id.txt)
         this.edt = findViewById(R.id.edt)
 
-        lifecycleScope.launch {
-            val response = MiscellaneousRepository().test231()
-        }
+        //AuthenticationService().setToken("eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI0IiwianRpIjoiNDJkZTAxYjkwMmNkYTQ4ZGY0ZDMyMTJhZGY4ZDZiNDAxOTI4NmYzMzUwZGQ4MGE1NTQxM2QxY2Q4NTNmNTM5MDgyZjYzNzIyNGJhYjBjNmQiLCJpYXQiOjE2NTU0MTIzMzIuMjU0NDY5LCJuYmYiOjE2NTU0MTIzMzIuMjU0NDcyLCJleHAiOjE2NTgwMDQzMzIuMTQ5ODU3LCJzdWIiOiIyMDMwMzEiLCJzY29wZXMiOltdfQ.GkGPiwRpGEJ7gPEEludI-_rU2F6LQj6wrC1Kfdhextr4AXsr3ZzIqxKLw0EJTSjcf9rxG4ZRV3fts7J_ki37Qju1olVRJRNC7R3eez15mOlZ3pmxTkXW6v-6HSsSzC9_rUraDy1yPAlBOBgIz6smjUHfzD-QbacHPN3PBN_W-6fwUifehoyCDo9oHUWxsv-KgY52kw4XVXlv_siXJBB0mmWZrjRoZ8nvA8CVeM7GfUUezSyUQW15VZWEHYU1X4Xaxxwczrh59760iMSw3XtgGVZTRANHWf4SGdUsTx76nVM-yJLXcHBjfNHuEbrmrkvnTEEl0DgkWWIxoMS_dGQKFZ62TJ-vh5Zw2ZexHABdBAhTEWp6d64nUhqdby1mMqYOWFkCPucC8HDq32usHKfDgchNe3tqmxixBB6iJ7J93oPonvrvajcrAogMGxsIqSwz03nxxH9Yhafma0z2WRuqkPH-8A6QLowTaeXREF619rxtqyrbTRp5jgu_UDTwIgSwTmp4ZRJXn7VnuHj9Rp8cJBdY3beNoOqBtnat8rSxLeAf1SItdNxQwymevvsc36ThtjIyUAUV6nRlb9hiWjyGY_i0A00elwUkgK6wPYJShTG_YiTWUOyaP11eDjT_LK-b0VnAD0t43ysRm6kzWp4JDqxpTex9w_mI3oDJhAxolMo")
+
+        /*lifecycleScope.launch {
+
+            val asd = AuthenticationService().forgotPasswordOTP(Login("+905357394980", ""))
+
+            if (asd.status == ResponseStatus.Success) {
+                Log.i("avs", "succ")
+            } else {
+                Log.i("avs", "non")
+            }
+
+            val responsea = AuthenticationService().login(Login("+905357394980", "394980"))
+
+            /*val response = HelpRepository().getLastTraining()
+
+            response.data?.training?.let {
+
+                Log.i("avs", "${it.meetingLink} ${it.endAt} ${it.startsAt}")
+
+                Log.i("avs", DateHelper.parseDate(it.startsAt, DateFormatType.dd_MMMM_yyyy))
+
+                Log.i("avs", DateHelper.parseDate(it.startsAt, DateFormatType.HH_mm))
+
+            }*/
+
+            Log.i("avs", responsea.data?.accessToken ?: "null token")
+
+        }*/
+
+        AuthenticationService().test()
 
     }
 
--- bifrost/build.gradle.kts
+++ bifrost/build.gradle.kts
@@ -1,9 +1,12 @@
+import com.android.build.gradle.internal.tasks.factory.dependsOn
+
 plugins {
     kotlin("multiplatform")
     id("com.android.library")
     id("maven-publish")
     id("com.chromaticnoise.multiplatform-swiftpackage") version "2.0.3"
     id("com.apollographql.apollo3").version("3.3.0")
+    id("com.google.firebase.crashlytics")
 }
 
 kotlin {
@@ -39,33 +42,26 @@ kotlin {
             api("org.awaitility:awaitility-kotlin:4.1.1")
             api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
             api("com.russhwolf:multiplatform-settings-no-arg:0.8.1")
+            api("com.soywiz.korlibs.klock:klock:2.2.0")
+            implementation("com.google.firebase:firebase-crashlytics:18.2.11")
+
         }
 
         sourceSets["androidMain"].dependencies {
             implementation("org.jetbrains.kotlin:kotlin-stdlib")
         }
 
-        val commonMain by getting
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test"))
-                implementation(kotlin("test-junit"))
-                implementation(kotlin("test-common"))
-                implementation(kotlin("test-annotations-common"))
-                implementation("org.jetbrains.kotlin:kotlin-test-common:1.7.0")
-                implementation("org.jetbrains.kotlin:kotlin-test-annotations-common:1.7.0")
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-common:1.3.0-M2")
-                implementation("io.mockk:mockk-common:1.9.3")
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
-            }
+        sourceSets["commonTest"].dependencies {
+            implementation(kotlin("test"))
         }
-        val androidTest by getting
+
     }
+
 }
 
 android {
 
-    //project.tasks.preBuild.dependsOn("graphqlSchemaDownloadTask")
+    project.tasks.preBuild.dependsOn("graphqlSchemaDownloadTask")
 
     compileSdk = 32
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
@@ -77,7 +73,7 @@ android {
 
 }
 
-/*tasks.register("graphqlSchemaDownloadTask") {
+tasks.register("graphqlSchemaDownloadTask") {
     doFirst {
         exec {
             commandLine(
@@ -86,7 +82,7 @@ android {
             )
         }
     }
-}*/
+}
 
 publishing {
     publications {
@@ -103,4 +99,5 @@ publishing {
 apollo {
     packageName.set("com.makswin.bifrost")
     generateKotlinModels.set(true)
+    generateOptionalOperationVariables.set(false)
 }
